f5b7cb18fc3a5142e12fd61378c99adb9fc28f17
lightclawjl <jeanlow@al.insper.edu.br>
2018-10-24 09:52:48 -0400

    foi; ta indo; passou; sim

---
+++ chocolate/README.ex
@@ -0,0 +1,7 @@
+# Overview
+Chocolate is a powerfull example made by following an online tutorial.
+#install
+juju deploy mysql
+juju deploy chocolate
+juju add-relation mysql chocolate
+juju expose chocolate
